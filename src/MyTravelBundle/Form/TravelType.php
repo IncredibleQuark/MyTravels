e506ac2d91bffb5b5fb97480e778cfcb4a09b051
IncredibleQuark <lkupinski@gmail.com>
2017-05-10 19:05:20 +0200

    Generaetd form type of travel entity

---
+++ src/MyTravelBundle/Form/TravelType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace MyTravelBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TravelType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('city')->add('country')->add('date')->add('lat')->add('lng')->add('type');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'MyTravelBundle\Entity\Travel'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'mytravelbundle_travel';
+    }
+
+
+}
