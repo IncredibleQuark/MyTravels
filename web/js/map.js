b9ea8310df4df30a894d6188d5f22fccb8b2d8b7
IncredibleQuark <lkupinski@gmail.com>
2017-05-10 14:55:56 +0200

    map display modifications

--- web/js/map.js
+++ web/js/map.js
@@ -2,17 +2,20 @@ var map;
 var marker;
 var infowindow;
 var messagewindow;
+var form =  document.getElementById('saveForm');
+var msg =  document.getElementById('successMessage');
 
 function initMap() {
     var california = {lat: 37.4419, lng: -122.1419};
     map = new google.maps.Map(document.getElementById('map'), {
         center: california,
-        zoom: 13
+        zoom: 4
     });
 
     infowindow = new google.maps.InfoWindow({
-        content: document.getElementById('form')
+        content: document.getElementById('saveForm')
     });
+
 
     messagewindow = new google.maps.InfoWindow({
         content: document.getElementById('message')
@@ -27,21 +30,25 @@ function initMap() {
 
         google.maps.event.addListener(marker, 'click', function() {
             infowindow.open(map, marker);
+            form.classList.remove('hidden');
         });
     });
 }
 
 function saveData() {
-    var name = escape(document.getElementById('name').value);
-    var address = escape(document.getElementById('address').value);
+    var city = encodeURI(document.getElementById('city').value);
+    var country = encodeURI(document.getElementById('country').value);
+    var date = encodeURI(document.getElementById('date').value);
     var type = document.getElementById('type').value;
     var latlng = marker.getPosition();
-    var url = 'phpsqlinfo_addrow.php?name=' + name + '&address=' + address +
+    var url = 'phpsqlinfo_addrow.php?city=' + city + '&country=' + country + '&date=' + date +
         '&type=' + type + '&lat=' + latlng.lat() + '&lng=' + latlng.lng();
+
+    msg.classList.remove('hidden');
 
     downloadUrl(url, function(data, responseCode) {
 
-        if (responseCode == 200 && data.length <= 1) {
+        if (responseCode === 200 && data.length <= 1) {
             infowindow.close();
             messagewindow.open(map, marker);
         }
