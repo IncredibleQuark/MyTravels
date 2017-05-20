71a4948756aedf521c75c2bb02b5f8ba6b278946
IncredibleQuark <lkupinski@gmail.com>
2017-05-20 20:11:12 +0200

    Improving edit info form

--- src/MyTravelBundle/Controller/UserInfoController.php
+++ src/MyTravelBundle/Controller/UserInfoController.php
@@ -8,41 +8,77 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class UserInfoController extends Controller
 {
     /**
      * @Route("profile/edit/newInfo/{id}")
-     * @Template("FOSUserBundle:Profile:info_form.html.twig")
+     * @Template("FOSUserBundle:Profile:edit_info.html.twig")
      * @Method("GET")
      */
     public function showNewInfoFormAction($id)
     {
-        $contact = new UserInfo();
+        $info = new UserInfo();
 
-        $form = $this->createForm(UserInfoType::class, $contact,
-            ['action' => $this->generateUrl('mytravel_userinfo_showeditform')]);
+        $form = $this->createForm(UserInfoType::class, $info,
+            ['action' => $this->generateUrl('mytravel_userinfo_createnewinfo')]);
 
         return ['form' => $form->createView()];
     }
 
+
     /**
-     * @Route("/profile/edit/{id}")
-     * @Template("FOSUserBundle:Profile:info_form.html.twig")
-     * @Method("GET")
+     * @Route("/profile/edit/NewInfo/create/new")
+     * @Template("@FOSUser/Profile/edit_info.html.twig")
+     * @Method("POST")
      */
-    public function showEditFormAction($id)
+    public function createNewInfoAction (Request $request)
     {
-        $contact = $this->getDoctrine()->getRepository('MyTravelBundle:UserInfo')->find($id);
+        $info = new UserInfo();
 
-        if (!$contact) {
+        $form = $this->createForm(UserInfoType::class, $info);
 
-            $this->redirectToRoute('mytravel_userinfo_shownewinfoform', array('id' => $id));
+//        $id = $this->getUser()->getId();
+//        $user = $this->getDoctrine()->getRepository('MyTravelBundle:User')->find($id);
 
+
+
+      //  $form->add('user', $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+
+            $em->persist($info);
+            $em->flush();
+
+            $id= $info->getId();
+
+            return $this->redirectToRoute('mytravel_userinfo_shownewinfoform', array('id' => $id));
         }
 
-        $form = $this->createForm(UserInfoType::class, $contact);
-
-        return ['form' => $form->createView(), 'id' => $id];
+        return ['form' => $form->createView()];
     }
+
+
+//    /**
+//     * @Route("/profile/editInfo/{id}")
+//     * @Method("GET")
+//     */
+//    public function showEditFormAction($id)
+//    {
+//        $contact = $this->getDoctrine()->getRepository('MyTravelBundle:UserInfo')->find($id);
+//
+//        if (!$contact) {
+//
+//            $this->redirectToRoute('mytravel_userinfo_shownewinfoform', array('id' => $id));
+//
+//        }
+//
+//        $form = $this->createForm(UserInfoType::class, $contact);
+//
+//        return ['form' => $form->createView(), 'id' => $id];
+//    }
 }
